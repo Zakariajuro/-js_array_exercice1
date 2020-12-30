09681d02fc5d0643e432376415091f5f38e96b1b
Zakaria <zakPro@live.be>
2020-12-30 14:38:24 +0100

    exo-fini

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+let coding = ["coding 15", "coding 16", "coding 17"]
+
+
+
+
+// console.log(coding);
+
+// console.log(coding.length);
+
+ console.log(coding[0]);
+ console.log(coding[1]);
+ console.log(coding[2]);
